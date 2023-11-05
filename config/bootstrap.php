4101c57d3572a7677f4e572389260ed93c25bfe1
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-11-05 11:22:03 -0300

    type: modify file file: config/bootstrap.php desciption:  session start in bootstrap file.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,7 +4,7 @@ use Core\Http\RequestFactory;
 use Core\Routes\Route;
 
 define('ROOT_PATH', dirname(__DIR__));
-
+session_start();
 require_once ROOT_PATH . '/vendor/autoload.php';
 
 require_once ROOT_PATH . '/config/routes.php';
