7d9c4657c4c9f87f5bea883bc6736102251c7b9c
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-11-11 16:50:20 -0300

    type: modify file file: config/routes.php description: update the route configuration.

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Filters\Config\OutputBufferizedFilter;
+use App\Filters\Security\CSRFFilter;
 use Core\Routes\Route;
 $rootRoute = new Route;
 $rootRoute->putErrorController('not_found', [App\Controllers\NotFoundController::class,'index']);
+$rootRoute->use(OutputBufferizedFilter::class);
+$rootRoute->use(CSRFFilter::class);
+$rootRoute->get('/login', [App\Controllers\LoginController::class, 'index']);
+$rootRoute->post('/login', [App\Controllers\LoginController::class, 'post']);
+$rootRoute->get('/signingup', [App\Controllers\SigningUpController::class, 'index']);
+$rootRoute->post('/signingup', [App\Controllers\SigningUpController::class, 'post']);
