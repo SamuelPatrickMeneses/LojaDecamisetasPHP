daaab98dc62ff593ae28bafcefc0ebb27021b5f8
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-11-13 15:15:30 -0300

    type: add service class file: App\Services\AdminService

---
+++ app/Services/AdminService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\DAOs\AdminDAO;
+use App\DAOs\UserDAO;
+use App\Entity\Admin;
+use App\Entity\User;
+use App\Exceptions\UserNotExistisException;
+
+class AdminService
+{
+    private AdminDAO $dao;
+    public function __construct()
+    {
+        $this->dao = new AdminDAO();
+    }
+
+    public function authenticate($name, $password)
+    {
+        try {
+            $admin =  $this->dao->findAdminByName($name);
+            if (password_verify($password, $admin->getPassword())) {
+                $_SESSION['admin'] = [];
+                $_SESSION['admin']['id'] = $admin->getId();
+                $_SESSION['admin']['name'] = $admin->getName();
+                $_SESSION['admin']['password'] = $admin->getPassword();
+                $_SESSION['loged'] = true;
+                return true;
+            } else {
+                return false;
+            }
+        } catch (UserNotExistisException $ex) {
+            return false;
+        }
+    }
+    public function logout()
+    {
+        unset($_SESSION['admin']);
+        $_SESSION['loged'] = false;
+    }
+}
