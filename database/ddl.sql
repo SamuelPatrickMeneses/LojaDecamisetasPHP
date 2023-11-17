e57e3033af6559826567e0d3a2db6c275134fdc0
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-11-17 14:50:33 -0300

    type: modify database file: database/ddl.sql description: add corelate tables products, grids, product_variants.

--- database/ddl.sql
+++ database/ddl.sql
@@ -19,3 +19,30 @@ CREATE TABLE IF NOT EXISTS admins(
   PRIMARY KEY (admin_id)
 ) CHARACTER SET=utf8;
 
+CREATE TABLE IF NOT EXISTS products (
+  product_id INT(11) NOT NULL AUTO_INCREMENT,
+  product_title VARCHAR(500) NOT NULL,
+  product_description VARCHAR(1000) NOT NULL,
+  product_price INT(6) NOT NULL,
+  PRIMARY KEY (product_id),
+  FULLTEXT (product_title)
+) CHARACTER SET=utf8;
+
+CREATE TABLE IF NOT EXISTS grids (
+  grid_id INT(11) NOT NULL AUTO_INCREMENT,
+  grid_size VARCHAR(3) NOT NULL,
+  grid_color VARCHAR(20) NOT NULL,
+  grid_label VARCHAR(26) NOT NULL,
+  grid_gender VARCHAR(1) NOT NULL,
+  PRIMARY KEY (grid_id)
+) CHARACTER SET=utf8;
+
+CREATE TABLE IF NOT EXISTS product_variants (
+  variant_id INT(11) NOT NULL AUTO_INCREMENT,
+  grid_id INT(11) NOT NULL,
+  product_id INT(11) NOT NULL,
+  price INT(6) NOT NULL,
+  stock_quantity INT(8) NOT NULL DEFAULT 0,
+  PRIMARY KEY (variant_id),
+  FOREIGN KEY (product_id) REFERENCES products(product_id)
+) CHARACTER SET=utf8;
