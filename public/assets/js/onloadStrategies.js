e4968178965c752164803e5e75e43dbad10f620d
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-12-04 19:45:26 -0300

    type: update frontiendiles files: 	public/assets/css/application.css 	public/assets/js/onloadStrategies.js descriptin: add alert syle and havior.

--- public/assets/js/onloadStrategies.js
+++ public/assets/js/onloadStrategies.js
@@ -1,10 +1,17 @@
 import productFrom from "./product.js";
 var dataCorelate = {};
+
 function loadDataCorelate() {
     let div = $('div.data-corelate');
     if (div.length === 1) {
         dataCorelate = JSON.parse(div.text());
     }
+}
+
+function closeAlerts() {
+    setTimeout(() => {
+        $(".alert").alert('close')
+    },5000)
 }
 
 
@@ -14,5 +21,6 @@ $(() => {
     );
     loadDataCorelate();
     productFrom(dataCorelate);
+    closeAlerts();
     $('.carousel').carousel('pause');
 })
