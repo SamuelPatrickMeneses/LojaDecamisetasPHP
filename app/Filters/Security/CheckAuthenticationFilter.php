044f3ff11bc6b5b94c9dc6f0bc051e58f3e7e06f
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-12-04 19:48:19 -0300

    type: add filter class class: App\Filters\Security\CheckAuthenticationFilter.php description: a siple accsses control mechanism.

---
+++ app/Filters/Security/CheckAuthenticationFilter.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\Filters\Security;
+
+use App\Lib\Flash;
+use Core\Http\Request;
+use Core\Routes\Filter;
+use Core\Routes\FilterChain;
+
+class CheckAuthenticationFilter implements Filter
+{
+    private $methods = [];
+
+    public function __construct(array $methods = [])
+    {
+        $this->methods = $methods;
+    }
+    public function doFilter(Request $requst, FilterChain $filterChain)
+    {
+        $redirect = in_array($requst->getMethod(), $this->methods)
+        && !isset($_SESSION['user']); 
+        if ($redirect) {
+            Flash::message('error_message', "do login to use uor service.");
+            header('location: /login');
+            exit();
+        }
+        $filterChain->next($requst);
+    }
+}
