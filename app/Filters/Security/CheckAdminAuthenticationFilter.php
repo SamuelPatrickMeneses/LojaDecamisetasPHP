329fb16bd212e4613ff62db60f3c4b46477c4ea0
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-12-07 10:42:04 -0300

    type: add filter class class: App\Filters\Security\CheckAdminAuthenticationFilter

---
+++ app/Filters/Security/CheckAdminAuthenticationFilter.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\Filters\Security;
+
+use App\Lib\Flash;
+use Core\Http\Request;
+use Core\Routes\Filter;
+use Core\Routes\FilterChain;
+
+class CheckAdminAuthenticationFilter implements Filter
+{
+    private $methods = [];
+
+    public function __construct(array $methods = [])
+    {
+        $this->methods = $methods;
+    }
+    public function doFilter(Request $requst, FilterChain $filterChain)
+    {
+        $redirect = in_array($requst->getMethod(), $this->methods)
+        && !isset($_SESSION['admin']); 
+        if ($redirect) {
+            Flash::message('error_message', "do login to use uor service.");
+            header('location: /admin/login');
+            exit();
+        }
+        $filterChain->next($requst);
+    }
+}
