bbcef151a198eb9727d7499a5e718fa02cb29208
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2024-03-30 15:10:15 -0300

    type: add test class class: Tests\Services\PublicKeyServiceTest

---
+++ tests/Services/PublicKeyServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Services;
+
+use App\Services\PublicKeyService;
+use PHPUnit\Framework\TestCase;
+
+class PublicKeyServiceTest extends TestCase
+{
+    private PublicKeyService $testad;
+
+    public function setup(): void
+    {
+        $this->testad = new PublickeyService();
+    }
+
+    public function testRequest()
+    {
+        $result = $this->testad->create();
+        TestCase::assertTrue(isset($result["public_key"]));
+    }
+}
